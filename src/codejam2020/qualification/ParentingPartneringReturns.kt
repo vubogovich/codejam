0473eaf846e7228f9f0db42076a9b786a7366c23
goldencham <vladimir.ubogovich@gmail.com>
2020-04-05 03:11:38 +0200

    Nice solution for 3rd problem of qualification round

--- src/codejam2020/qualification/ParentingPartneringReturns.kt
+++ src/codejam2020/qualification/ParentingPartneringReturns.kt
@@ -1,42 +1,31 @@
 package codejam2020.qualification
 
-fun findOption(activities: Array<IntRange>, timelineC: MutableSet<Int>, timelineJ: MutableSet<Int>, assignment: CharArray): Boolean {
-    val index = assignment.indexOf(' ')
-    if (index < 0) return true
-
-    if (!activities[index].any { timelineC.contains(it) }) {
-        timelineC.addAll(activities[index])
-        assignment[index] = 'C'
-        if (findOption(activities, timelineC, timelineJ, assignment)) return true
-        assignment[index] = ' '
-        timelineC.removeAll(activities[index])
-    }
-
-    if (!activities[index].any { timelineJ.contains(it) }) {
-        timelineJ.addAll(activities[index])
-        assignment[index] = 'J'
-        if (findOption(activities, timelineC, timelineJ, assignment)) return true
-        assignment[index] = ' '
-        timelineJ.removeAll(activities[index])
-    }
-
-    return false
-}
-
 fun main() {
     val inputFileName = "src/codejam2020/qualification/ParentingPartneringReturns.in"
     java.io.File(inputFileName).takeIf { it.exists() }?.also { System.setIn(it.inputStream()) }
 
     for (case in 1..readLine()!!.toInt()) {
-        val activities = Array(readLine()!!.toInt()) {
-            readLine()!!.split(' ').map { it.toInt() }.let { it[0] until it[1] }
+        val activities = Array(readLine()!!.toInt()) { index ->
+            readLine()!!.split(' ').map { it.toInt() }.let { Activity(index, it[0] until it[1]) }
         }
 
-        val timelineC = mutableSetOf<Int>()
-        val timelineJ = mutableSetOf<Int>()
-        val assignment = CharArray(activities.size) { ' ' }
-        val exists = findOption(activities, timelineC, timelineJ, assignment)
-        val result = if (exists) assignment.joinToString("") else "IMPOSSIBLE"
+        val timelines = mapOf(
+            'C' to mutableSetOf<Int>(),
+            'J' to mutableSetOf<Int>()
+        )
+
+        val assignment = CharArray(activities.size) { 'X' }
+
+        activities.sortedBy { it.range.start }.forEach { activity ->
+            listOf('C', 'J').find { !timelines[it]!!.contains(activity.range.start) }?.let {
+                assignment[activity.index] = it
+                timelines[it]!!.addAll(activity.range)
+            }
+        }
+
+        val result = if (assignment.contains('X')) "IMPOSSIBLE" else assignment.joinToString("")
         println("Case #$case: ${result}")
     }
 }
+
+data class Activity(val index: Int, val range: IntRange)
